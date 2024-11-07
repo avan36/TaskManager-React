9d3409e5d9ac88eb3cca92efd4cf5303f2cae144
avan36 <158532304+avan36@users.noreply.github.com>
2024-11-06 20:40:55 -0500

    Main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
+import { useState } from 'react';
+import { Task } from "./Task";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value); //takes in event and sets the new task as the event.target.value
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1, //We want the id to increment by 1
+      taskName: newTask,
+      completed: false
+    }
+    
+    setTodoList([...todoList, task]);
+  }
+
+  const deleteTask = (id) => {
+    const newTodoList = todoList.filter((task) => {
+      return task.id != id
+    });
+    setTodoList(newTodoList);
+  };
+
+  const completeTask = (id) => {
+    setTodoList (
+      todoList.map((task) => {
+        if (task.id == id) {
+          if (task.completed) {
+            return {... task, completed: false}
+          }
+          else {
+            return {... task, completed: true}
+          }
+        } else {
+          return task;
+        }
+      })
+
+    );
+  };
+
+  return (<div className='App'>
+    <div className = 'addTask'>
+      <input onChange={handleChange}/>
+      <button onClick={addTask}>Add task</button>
+    </div>
+
+    <div className= 'list'></div>
+    {todoList.map((current_task) =>  {
+      return (<Task taskName = {current_task.taskName} id = {current_task.id} completed = {current_task.completed} deleteTask = {deleteTask} completeTask = {completeTask}/> );
+    })}
     </div>
   );
 }
-
 export default App;
